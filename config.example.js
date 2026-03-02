9a37a34ac3ec916f0c33c85dd1daf16e52d5d58f
Marien van Overbeek <git@marienvanoverbeek.nl>
2026-03-02 19:47:44 +0100

    rm calendar.json

--- config.example.js
+++ config.example.js
@@ -5,3 +5,4 @@ export const R2_ACCOUNT_ID = 'your-cloudflare-account-id';
 export const R2_ACCESS_KEY_ID = 'your-r2-access-key-id';
 export const R2_SECRET_ACCESS_KEY = 'your-r2-secret-access-key';
 export const R2_BUCKET_NAME = 'your-r2-bucket-name';
+export const ICS_URL = "your-ics-url";
